a7bce5fe4ff24f4ff5d7f10afc4dde6d8a9ce153
byeagley <46436812+byeagley@users.noreply.github.com>
2021-04-08 13:20:22 -0400

    custom reaction to factors in early stages

--- PhysiCell/custom_modules/custom.h
+++ PhysiCell/custom_modules/custom.h
@@ -90,3 +90,5 @@ void custom_function( Cell* pCell, Phenotype& phenotype , double dt );
 
 void contact_function( Cell* pMe, Phenotype& phenoMe , Cell* pOther, Phenotype& phenoOther , double dt ); 
 
+void chemo_phenotype( Cell* pCell, Phenotype& phenotype, double dt);
+
